e4a1c20dbc19683411c46a2300d8a6d8d8c48ae7
Peck <peck@localhost>
2016-10-16 19:13:52 +0200

    first functions C-compiler

--- ElasticPL/ElasticPLInterpreter.c
+++ ElasticPL/ElasticPLInterpreter.c
@@ -397,16 +397,16 @@ extern char* compile(ast* exp) {
 				
 		switch (exp->type) {
 		case NODE_CONSTANT:
-			
+			sprintf(result,"%d",exp->value);
 			break;
 		case NODE_VAR_CONST:
-			
+			sprintf(result,"mem[%d]",exp->value);
 			break;
 		case NODE_VAR_EXP:
 			
 			break;
 		case NODE_ASSIGN:
-			
+			sprintf(result,"%s = %s;\n",lval,rval);
 			break;
 		case NODE_IF:
 			
@@ -462,7 +462,7 @@ extern char* compile(ast* exp) {
 	
 			break;
 		case NODE_EQ:
-			
+			sprintf(result,"((%s) == (%s))",lval,rval);
 			break;
 		case NODE_NE:
 			
@@ -492,7 +492,7 @@ extern char* compile(ast* exp) {
 			applog(LOG_ERR, "ERROR: VM Runtime - MD5 Not Implemented");
 			return NULL;
 		case NODE_VERIFY:
-		
+			sprintf(result,"return %s;\n",lval);
 			break;
 		default:
 			applog(LOG_ERR, "ERROR: VM Runtime - Unsupported Operation (%d)", exp->type);
