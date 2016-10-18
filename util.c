eb8d2941b3373534655d1405c87dc8eb39d043f1
Sprocket <cryptoflyr@gmail.com>
2016-10-18 09:55:20 -0400

    cleanup compatibility issues

--- util.c
+++ util.c
@@ -10,6 +10,9 @@
 * any later version.
 */
 
+#define _GNU_SOURCE
+
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdarg.h>
@@ -294,61 +297,6 @@ static size_t all_data_cb(const void *ptr, size_t size, size_t nmemb, void *user
 	memcpy((unsigned char*)db->buf + newlen, &zero, 1);	/* null terminate */
 
 	return len;
-}
-
-static size_t resp_hdr_cb(void *ptr, size_t size, size_t nmemb, void *user_data)
-{
-	struct header_info *hi = (struct header_info *) user_data;
-	size_t remlen, slen, ptrlen = size * nmemb;
-	char *rem, *val = NULL, *key = NULL;
-	void *tmp;
-
-	val = (char*)calloc(1, ptrlen);
-	key = (char*)calloc(1, ptrlen);
-	if (!key || !val)
-		goto out;
-
-	tmp = memchr(ptr, ':', ptrlen);
-	if (!tmp || (tmp == ptr))	/* skip empty keys / blanks */
-		goto out;
-	slen = (char*)tmp - (char*)ptr;
-	if ((slen + 1) == ptrlen)	/* skip key w/ no value */
-		goto out;
-	memcpy(key, ptr, slen);		/* store & nul term key */
-	key[slen] = 0;
-
-	rem = (char*)ptr + slen + 1;		/* trim value's leading whitespace */
-	remlen = ptrlen - slen - 1;
-	while ((remlen > 0) && (isspace(*rem))) {
-		remlen--;
-		rem++;
-	}
-
-	memcpy(val, rem, remlen);	/* store value, trim trailing ws */
-	val[remlen] = 0;
-	while ((*val) && (isspace(val[strlen(val) - 1]))) {
-		val[strlen(val) - 1] = 0;
-	}
-
-	if (!strcasecmp("X-Long-Polling", key)) {
-		hi->lp_path = val;	/* steal memory reference */
-		val = NULL;
-	}
-
-	if (!strcasecmp("X-Reject-Reason", key)) {
-		hi->reason = val;	/* steal memory reference */
-		val = NULL;
-	}
-
-	if (!strcasecmp("X-Stratum", key)) {
-		hi->stratum_url = val;	/* steal memory reference */
-		val = NULL;
-	}
-
-out:
-	free(key);
-	free(val);
-	return ptrlen;
 }
 
 json_t* json_rpc_call(CURL *curl, const char *url, const char *userpass, const char *req, int *curl_err) {
