4485f449780597b59138e09470a1425e16d632d1
Sprocket <cryptoflyr@gmail.com>
2016-12-21 09:33:53 -0500

    fixed compiled C code calls to gmp

--- ElasticPL/ElasticPLFunctions.h
+++ ElasticPL/ElasticPLFunctions.h
@@ -10,11 +10,6 @@
 #ifndef ELASTICPLFUNCTIONS_H_
 #define ELASTICPLFUNCTIONS_H_
 
-// The following is needed to override a gmp error
-#ifdef _MSC_VER
-#pragma warning(disable:4146)
-#endif
-
 #include <stdint.h>
 
 #include <openssl/md5.h>
@@ -40,36 +35,36 @@ extern uint32_t epl_ec_mult(size_t idx1, bool comp1, size_t idx2, size_t n2, boo
 
 extern int32_t gcd(int32_t	a, int32_t b);
 
-extern void big_init_const(mpz_t* out, unsigned char* str);
-extern void big_init_expr(mpz_t* out, int32_t	a);
-extern void big_add(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_sub(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_mul(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_div(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_ceil_div(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_floor_div(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_truncate_div(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_div_exact(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_mod(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_neg(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_lshift(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_rshift(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_gcd(mpz_t* out, mpz_t* a, mpz_t* b);
-extern int32_t big_divisible(mpz_t* a, mpz_t* b);
-extern void big_congruent_mod_p(mpz_t* a, mpz_t* b, mpz_t* p);
-extern void big_pow(mpz_t* out, mpz_t* a, uint32_t b);
-extern void big_pow2(mpz_t* out, uint32_t b);
-extern void big_pow_mod_p(mpz_t* out, mpz_t* a, mpz_t* b, mpz_t* c);
-extern void big_pow2_mod_p(mpz_t* out, mpz_t* a, mpz_t* b);
-extern int32_t big_compare(mpz_t* a, mpz_t* b);
-extern int32_t big_compare_abs(mpz_t* a, mpz_t* b);
-extern int32_t big_sign(mpz_t* a);
-extern void big_or(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_and(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_xor(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_or_integer(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_and_integer(mpz_t* out, mpz_t* a, mpz_t* b);
-extern void big_xor_integer(mpz_t* out, mpz_t* a, mpz_t* b);
-extern int big_least_32bit(mpz_t* a);
+extern void big_init_const(mpz_t out, unsigned char* str);
+extern void big_init_expr(mpz_t out, int32_t	a);
+extern void big_add(mpz_t out, mpz_t a, mpz_t b);
+extern void big_sub(mpz_t out, mpz_t a, mpz_t b);
+extern void big_mul(mpz_t out, mpz_t a, mpz_t b);
+extern void big_div(mpz_t out, mpz_t a, mpz_t b);
+extern void big_ceil_div(mpz_t out, mpz_t a, mpz_t b);
+extern void big_floor_div(mpz_t out, mpz_t a, mpz_t b);
+extern void big_truncate_div(mpz_t out, mpz_t a, mpz_t b);
+extern void big_div_exact(mpz_t out, mpz_t a, mpz_t b);
+extern void big_mod(mpz_t out, mpz_t a, mpz_t b);
+extern void big_neg(mpz_t out, mpz_t a, mpz_t b);
+extern void big_lshift(mpz_t out, mpz_t a, mpz_t b);
+extern void big_rshift(mpz_t out, mpz_t a, mpz_t b);
+extern void big_gcd(mpz_t out, mpz_t a, mpz_t b);
+extern int32_t big_divisible(mpz_t a, mpz_t b);
+extern void big_congruent_mod_p(mpz_t a, mpz_t b, mpz_t p);
+extern void big_pow(mpz_t out, mpz_t a, uint32_t b);
+extern void big_pow2(mpz_t out, uint32_t b);
+extern void big_pow_mod_p(mpz_t out, mpz_t a, mpz_t b, mpz_t c);
+extern void big_pow2_mod_p(mpz_t out, mpz_t a, mpz_t b);
+extern int32_t big_compare(mpz_t a, mpz_t b);
+extern int32_t big_compare_abs(mpz_t a, mpz_t b);
+extern int32_t big_sign(mpz_t a);
+extern void big_or(mpz_t out, mpz_t a, mpz_t b);
+extern void big_and(mpz_t out, mpz_t a, mpz_t b);
+extern void big_xor(mpz_t out, mpz_t a, mpz_t b);
+extern void big_or_integer(mpz_t out, mpz_t a, mpz_t b);
+extern void big_and_integer(mpz_t out, mpz_t a, mpz_t b);
+extern void big_xor_integer(mpz_t out, mpz_t a, mpz_t b);
+extern int big_least_32bit(mpz_t a);
 
 #endif // ELASTICPLFUNCTIONS_H_
--- ElasticPL/ElasticPLMath.c
+++ ElasticPL/ElasticPLMath.c
@@ -7,11 +7,6 @@
 * any later version.
 */
 
-// The following is needed to override a gmp error
-#ifdef _MSC_VER
-#pragma warning(disable:4146)
-#endif
-
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -31,112 +26,112 @@ extern int32_t gcd(int32_t a, int32_t b) {
 	return a;
 }
 
-extern void big_init_const(mpz_t* out, unsigned char* str) {
+extern void big_init_const(mpz_t out, unsigned char* str) {
 
 	if (!str)
 		return;
 
 	if (str[0] == '0' && str[1] == 'x' && strlen(str) > 2 && strlen(str) <= 66) {
-		mpz_init_set_str(*out, str + 2, 16);
+		mpz_init_set_str(out, str + 2, 16);
 	}
 	else if (str[0] == '0' && str[1] == 'b' && strlen(str) > 2 && strlen(str) <= 66) {
-		mpz_init_set_str(*out, str + 2, 2);
+		mpz_init_set_str(out, str + 2, 2);
 	}
 	else {
-		mpz_init_set_str(*out, str, 10);
+		mpz_init_set_str(out, str, 10);
 	}
 }
 
-extern void big_init_expr(mpz_t* out, int32_t a) {
+extern void big_init_expr(mpz_t out, int32_t a) {
 	char str[15];
 	sprintf(str, "%d", a);
-	mpz_init_set_str(*out, str, 10);
+	mpz_init_set_str(out, str, 10);
 }
 
-extern void big_add(mpz_t* out, mpz_t* a, mpz_t* b) {
-	mpz_add(*out, *a, *b);
+extern void big_add(mpz_t out, mpz_t a, mpz_t b) {
+	mpz_add(out, a, b);
 }
-extern void big_sub(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_sub(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_mul(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_mul(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_div(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_div(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_ceil_div(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_ceil_div(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_floor_div(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_floor_div(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_truncate_div(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_truncate_div(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_div_exact(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_div_exact(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_mod(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_mod(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_neg(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_neg(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_lshift(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_lshift(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_rshift(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_rshift(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_gcd(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_gcd(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern int32_t big_divisible(mpz_t* a, mpz_t* b) {
+extern int32_t big_divisible(mpz_t a, mpz_t b) {
 	return 0;
 }
-extern void big_congruent_mod_p(mpz_t* a, mpz_t* b, mpz_t* p) {
+extern void big_congruent_mod_p(mpz_t a, mpz_t b, mpz_t p) {
 	;
 }
-extern void big_pow(mpz_t* out, mpz_t* a, uint32_t b) {
+extern void big_pow(mpz_t out, mpz_t a, uint32_t b) {
 	;
 }
-extern void big_pow2(mpz_t* out, uint32_t b) {
+extern void big_pow2(mpz_t out, uint32_t b) {
 	;
 }
-extern void big_pow_mod_p(mpz_t* out, mpz_t* a, mpz_t* b, mpz_t* c) {
+extern void big_pow_mod_p(mpz_t out, mpz_t a, mpz_t b, mpz_t c) {
 	;
 }
-extern void big_pow2_mod_p(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_pow2_mod_p(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern int32_t big_compare(mpz_t* a, mpz_t* b) {
+extern int32_t big_compare(mpz_t a, mpz_t b) {
 	return 0;
 }
-extern int32_t big_compare_abs(mpz_t* a, mpz_t* b) {
+extern int32_t big_compare_abs(mpz_t a, mpz_t b) {
 	return 0;
 }
-extern int32_t big_sign(mpz_t* a) {
+extern int32_t big_sign(mpz_t a) {
 	return 0;
 }
-extern void big_or(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_or(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_and(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_and(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_xor(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_xor(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_or_integer(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_or_integer(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_and_integer(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_and_integer(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern void big_xor_integer(mpz_t* out, mpz_t* a, mpz_t* b) {
+extern void big_xor_integer(mpz_t out, mpz_t a, mpz_t b) {
 	;
 }
-extern int32_t big_least_32bit(mpz_t* a) {
+extern int32_t big_least_32bit(mpz_t a) {
 	return 0;
 }
